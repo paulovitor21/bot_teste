f16761c93845a106a2b8b5ad2f64f580c2cd6ea4
Paulo Vitor <80797702+paulovitor21@users.noreply.github.com>
2023-11-10 20:15:54 -0400

    link para inspecionar pagina

--- bot_selenium.py
+++ bot_selenium.py
@@ -7,14 +7,17 @@ msedge_driver_path = 'IEDriverServer.exe'
 # Configuração do Internet Explorer Options
 ie_options = IeOptions()
 ie_options.add_argument("--ie=edge")  # Configura o modo de compatibilidade com o Edge
+ie_options.add_argument("--silent")   # Adiciona o argumento para evitar avisos
 
 # Cria uma instância do WebDriver do Internet Explorer
 driver = webdriver.Ie(executable_path=msedge_driver_path, options=ie_options)
 
 # Exemplo de uso: abrindo uma página
-driver.get("https://www.google.com")
+driver.get("https://learn.microsoft.com/pt-br/microsoft-edge/devtools-guide-chromium/ie-mode/")
 
 # Restante do seu código...
 
 # Fecha o navegador
 #driver.quit()
+
+#link de como inspecionar pagina no modo IE do edge: https://learn.microsoft.com/pt-br/microsoft-edge/devtools-guide-chromium/ie-mode/
